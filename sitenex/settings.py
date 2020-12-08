bcf0395e6293b7d6aab09e9841abdfbe60beabe9
brjatoba92 <brjatoba92@gmail.com>
2020-12-08 12:58:36 -0300

    Secret key
    close #23

--- sitenex/settings.py
+++ sitenex/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zscppz_q3zq0hcown6!1z!)x%j15=0zn7^19w=kis1ibdc-$sg'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
