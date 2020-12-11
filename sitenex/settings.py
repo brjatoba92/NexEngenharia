6a3942ecd457c1dc613cca9d89e7132fbad08493
brjatoba92 <brjatoba92@gmail.com>
2020-12-10 23:27:51 -0300

    Iniciando coleta de arquivos estaticos
    close #29

--- sitenex/settings.py
+++ sitenex/settings.py
@@ -109,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = config('LANGUAGE_CODE', default='pt-br')
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = config('TIME ZONE', default='America/Sao_Paulo')
 
 USE_I18N = True
 
@@ -124,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
